c6f573a7da6e0fce09e2d9ed374becada4966307
Yantestes12 <tioninja14@gmail.com>
2026-08-31 14:42:28 -0300

    update files

--- site_integra_central/fix_script_tag.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-let areaAlunoPath = 'area_aluno.html';
-let areaHtml = fs.readFileSync(areaAlunoPath, 'utf8');
-
-// Escape the closing script tag inside the JS string literals
-areaHtml = areaHtml.replace(/<\/script><\/body><\/html>'\);/g, '<\\/script></body></html>\');');
-
-fs.writeFileSync(areaAlunoPath, areaHtml);
--- site_integra_central/fix_translate_matricula.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-let filePath = 'matricula.html';
-let f = fs.readFileSync(filePath, 'utf8');
-
-f = f.replace(/<body>/g, '<body class="notranslate" translate="no">');
-f = f.replace(/class="material-symbols-outlined"/g, 'class="material-symbols-outlined notranslate" translate="no"');
-f = f.replace(/class='material-symbols-outlined'/g, "class='material-symbols-outlined notranslate' translate='no'");
-
-fs.writeFileSync(filePath, f);
